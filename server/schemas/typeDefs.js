735a5367862a4f1d62dc09f2472951ac837919e3
Matt F <fiaschettima@gmail.com>
2022-08-29 21:55:42 -0400

    type user def

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,17 @@
 const { gql } = require("apollo-server-express");
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+        _id : ID!
+        username : String
+        email : String
+        password : String
+        bookCount: Int
+        savedBooks : [Book]
+    }
+
+    type Book {
+        
+    }
+`;
 
 module.exports = typeDefs;
