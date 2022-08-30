7682da99b192c144e2189cd4fe6666a23fd14dd8
Matt F <fiaschettima@gmail.com>
2022-08-29 22:39:20 -0400

    client side mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,21 +1,53 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-    query singleUser {
-        user {
+export const NEW_USER = gql `
+    mutation newUser($username : String!, $email: String!, $password: String!){
+        newUser(username: $username, email: $email, password: $password){token}
+            user{
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    _id
+                    bookId
+                    authors
+                    description
+                    title
+                    link
+                    image
+                }
+            }
+    }
+`;
+
+export const USER_LOGIN = gql`
+    mutation login(email:String!, password : String!){
+        login(email: $email, password: $password){token}
+            user{
+                _id
+                username
+                email
+            }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook(input: savedBook!){
+        saveBook(input: $input){
             _id
             username 
             email
-            password
             bookCount
-            savedBooks {
-                _id bookId
-                authors 
+            savedBooks{
+                _id
+                authors
                 description
-                title 
-                link
+                bookId
                 image
+                Link
+                title
             }
         }
     }
-`
+`;
