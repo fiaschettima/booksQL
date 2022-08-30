f92747b09d5b6b4a463eab116c338428a87e3e89
Matt F <fiaschettima@gmail.com>
2022-08-29 22:40:34 -0400

    delete mutation client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -51,3 +51,23 @@ export const SAVE_BOOK = gql`
         }
     }
 `;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook(bookId: ID!){
+        deleteBook(bookId: $bookId){
+            _id
+            username 
+            email
+            bookCount
+            savedBooks{
+                _id
+                authors
+                description
+                bookId
+                image
+                Link
+                title
+            }
+        }
+    }
+`;
